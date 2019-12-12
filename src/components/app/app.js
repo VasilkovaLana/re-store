0976582f614138ccae17d3acb6e72a7ff565a8e6
VasilkovaLana <45256974+VasilkovaLana@users.noreply.github.com>
2019-12-12 15:17:10 +0300

    adding syles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
+import ShopHeader from '../shop-header/shop-header';
 import './app.css';
 
 const App = () => {
   return (
-    <Switch>
-      <Route
-        path='/'
-        component={HomePage}
-        exact />
+    <main role="main" className="conteiner">
+      <ShopHeader numItems={5} total={210} />
+      <Switch>
+        <Route
+          path='/'
+          component={HomePage}
+          exact />
 
-      <Route
-        path='/cart'
-        component={CartPage} />
-    </Switch>
+        <Route
+          path='/cart'
+          component={CartPage} />
+      </Switch>
+    </main>
   )
 };
 
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,12 +1,18 @@
 export default class BookstoreService {
   getBooks() {
     return [
-      { id: 1,
-        title: 'You Don\'t Know JS',
-        author: 'Kyle Simpson' },
-      { id: 2,
+      {
+         id: 1,
         title: 'Ten Arguments for Deleting Your Social Media Accounts Right Now',
-        author: 'Jaron Lanier' }
+        author: 'Jaron Lanier',
+        price: '13.00',
+        coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41KVyUJTmjL.jpg' },
+      { 
+        id: 2,
+        title: 'You Don\'t Know JS: ES6 & Beyond',
+        author: 'Kyle Simpson',
+        price: '10.99',
+        coverImage: 'https://images-na.ssl-images-amazon.com/images/I/410%2BtmN-P%2BL._SX331_BO1,204,203,200_.jpg' }
     ];
   }
 }
